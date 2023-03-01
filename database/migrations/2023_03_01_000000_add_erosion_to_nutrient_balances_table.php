b174815c9ddb02a0b8e0c433f1c07c2d84a6dc03
Dave Mills <d.e.mills@stats4sd.org>
2023-03-01 12:08:37 +0000

    hotfix for migration naming

--- database/migrations/2023_03_01_000000_add_erosion_to_nutrient_balances_table.php
+++ database/migrations/2023_03_01_000000_add_erosion_to_nutrient_balances_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class MakeNullablesSamplesTable extends Migration
+class AddErosionToNutrientBalancesTable extends Migration
 {
     /**
      * Run the migrations.
