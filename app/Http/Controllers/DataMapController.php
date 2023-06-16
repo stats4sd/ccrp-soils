4f7bbcccfe1ce73bd13671638152b0a76dadb6b0
Dan Tang <dan@stats4sd.org>
2023-06-16 16:06:45 +0100

    Quick fix to import nutrient balance data for demo on 2023-06-19, more to discuss with Steve for the delta change

--- app/Http/Controllers/DataMapController.php
+++ app/Http/Controllers/DataMapController.php
@@ -197,6 +197,42 @@ class DataMapController extends Controller
     }
 
 
+    // handle array for repeated group
+    public static function handleRepeat($repeatData, $projectId, $farmerFieldId)
+    {
+        foreach ($repeatData as $repeat) {
+            // TODO: need to check with Steve Vanek for how to calculate year
+            // as a temporary workaround, just assume we still use an_4,3,2,1 to represent year 2020,2019,2018,2017 first
+            $year = 2021 - $repeat['crop_repeat_group/year_pos'];
+
+            NutrientBalance::create([
+                'project_id' => $projectId,
+                'farmer_field_id' => $farmerFieldId,
+                'year' => $year,
+                'erosion_level' => $repeat['crop_repeat_group/erosion_level_anX'] ?? null,
+                'erosion_amt_Tha' => $repeat['crop_repeat_group/erosion_amt_Tha_anX'] ?? null,
+                'erosionNloss_kgHa' => $repeat['crop_repeat_group/erosionNloss_kgHa_anX'] ?? null,
+                'erosionPloss_kgHa' => $repeat['crop_repeat_group/erosionPloss_kgHa_anX'] ?? null,
+                'erosionKloss_kgHa' => $repeat['crop_repeat_group/erosionKloss_kgHa_anX'] ?? null,
+                'tot_org_Ninput' => $repeat['crop_repeat_group/tot_org_Ninput_anX'] ?? null,
+                'tot_org_Pinput' => $repeat['crop_repeat_group/tot_org_Pinput_anX'] ?? null,
+                'tot_org_Kinput' => $repeat['crop_repeat_group/tot_org_Kinput_anX'] ?? null,
+                'tot_inorg_Ninput' => $repeat['crop_repeat_group/tot_inorg_Ninput_anX'] ?? null,
+                'tot_inorg_Pinput' => $repeat['crop_repeat_group/tot_inorg_Pinput_anX'] ?? null,
+                'tot_inorg_Kinput' => $repeat['crop_repeat_group/tot_inorg_Kinput_anX'] ?? null,
+                'Total_cropNexport' => $repeat['crop_repeat_group/Total_cropNexport_anX'] ?? null,
+                'Total_cropPexport' => $repeat['crop_repeat_group/Total_cropPexport_anX'] ?? null,
+                'Total_cropKexport' => $repeat['crop_repeat_group/Total_cropKexport_anX'] ?? null,
+                'balance_N' => $repeat['crop_repeat_group/balance_N_anX'] ?? null,  
+                'balance_P' => $repeat['crop_repeat_group/balance_P_anX'] ?? null,
+                'balance_K' => $repeat['crop_repeat_group/balance_K_anX'] ?? null,
+            ]);
+
+        }
+    }
+
+
+
     public static function handleNutrients($data, $projectId)
     {
         $farmerField = FarmerField::create([
@@ -209,103 +245,117 @@ class DataMapController extends Controller
             'size' => $data['field_size_final3'] ?? null,
         ]);
 
-        NutrientBalance::create([
-            'project_id' => $projectId,
-            'farmer_field_id' => $farmerField->id,
-            'year' => '2020',
-            'erosion_level' => $data['erosion_level_an4'] ?? null,
-            'erosion_amt_Tha' => $data['erosion_amt_Tha_an4'] ?? null,
-            'erosionNloss_kgHa' => $data['erosionNloss_kgHa_an4'] ?? null,
-            'erosionPloss_kgHa' => $data['erosionPloss_kgHa_an4'] ?? null,
-            'erosionKloss_kgHa' => $data['erosionKloss_kgHa_an4'] ?? null,
-            'tot_org_Ninput' => $data['tot_org_Ninput_an4'] ?? null,
-            'tot_org_Pinput' => $data['tot_org_Pinput_an4'] ?? null,
-            'tot_org_Kinput' => $data['tot_org_Kinput_an4'] ?? null,
-            'tot_inorg_Ninput' => $data['tot_inorg_Ninput_an4'] ?? null,
-            'tot_inorg_Pinput' => $data['tot_inorg_Pinput_an4'] ?? null,
-            'tot_inorg_Kinput' => $data['tot_inorg_Kinput_an4'] ?? null,
-            'Total_cropNexport' => $data['Total_cropNexport_an4'] ?? null,
-            'Total_cropPexport' => $data['Total_cropPexport_an4'] ?? null,
-            'Total_cropKexport' => $data['Total_cropKexport_an4'] ?? null,
-            'balance_N' => $data['balance_N_an4'] ?? null,
-            'balance_P' => $data['balance_P_an4'] ?? null,
-            'balance_K' => $data['balance_K_an4'] ?? null,
-        ]);
 
-        if (isset($data['additional_an3']) && $data['additional_an3'] === 'yes') {
+        if (isset($data['crop_repeat_group'])) {
+            // to support new ODK form
+
+            // handle repeat group "crop_repeat_group"
+            DataMapController::handleRepeat($data['crop_repeat_group'], $projectId, $farmerField->id);
+
+        } else {
+            // to support old ODK form
+            // suppose we can remove this code segment when all projects are using the new ODK form,
+            // keep this code segment temporary for smooth demo on 2023-06-19
+
             NutrientBalance::create([
                 'project_id' => $projectId,
                 'farmer_field_id' => $farmerField->id,
-                'year' => '2019',
-                'erosion_level' => $data['erosion_level_an3'] ?? null,
-                'erosion_amt_Tha' => $data['erosion_amt_Tha_an3'] ?? null,
-                'erosionNloss_kgHa' => $data['erosionNloss_kgHa_an3'] ?? null,
-                'erosionPloss_kgHa' => $data['erosionPloss_kgHa_an3'] ?? null,
-                'erosionKloss_kgHa' => $data['erosionKloss_kgHa_an3'] ?? null,
-                'tot_org_Ninput' => $data['tot_org_Ninput_an3'] ?? null,
-                'tot_org_Pinput' => $data['tot_org_Pinput_an3'] ?? null,
-                'tot_org_Kinput' => $data['tot_org_Kinput_an3'] ?? null,
-                'tot_inorg_Ninput' => $data['tot_inorg_Ninput_an3'] ?? null,
-                'tot_inorg_Pinput' => $data['tot_inorg_Pinput_an3'] ?? null,
-                'tot_inorg_Kinput' => $data['tot_inorg_Kinput_an3'] ?? null,
-                'Total_cropNexport' => $data['Total_cropNexport_an3'] ?? null,
-                'Total_cropPexport' => $data['Total_cropPexport_an3'] ?? null,
-                'Total_cropKexport' => $data['Total_cropKexport_an3'] ?? null,
-                'balance_N' => $data['balance_N_an3'] ?? null,
-                'balance_P' => $data['balance_P_an3'] ?? null,
-                'balance_K' => $data['balance_K_an3'] ?? null,
+                'year' => '2020',
+                'erosion_level' => $data['erosion_level_an4'] ?? null,
+                'erosion_amt_Tha' => $data['erosion_amt_Tha_an4'] ?? null,
+                'erosionNloss_kgHa' => $data['erosionNloss_kgHa_an4'] ?? null,
+                'erosionPloss_kgHa' => $data['erosionPloss_kgHa_an4'] ?? null,
+                'erosionKloss_kgHa' => $data['erosionKloss_kgHa_an4'] ?? null,
+                'tot_org_Ninput' => $data['tot_org_Ninput_an4'] ?? null,
+                'tot_org_Pinput' => $data['tot_org_Pinput_an4'] ?? null,
+                'tot_org_Kinput' => $data['tot_org_Kinput_an4'] ?? null,
+                'tot_inorg_Ninput' => $data['tot_inorg_Ninput_an4'] ?? null,
+                'tot_inorg_Pinput' => $data['tot_inorg_Pinput_an4'] ?? null,
+                'tot_inorg_Kinput' => $data['tot_inorg_Kinput_an4'] ?? null,
+                'Total_cropNexport' => $data['Total_cropNexport_an4'] ?? null,
+                'Total_cropPexport' => $data['Total_cropPexport_an4'] ?? null,
+                'Total_cropKexport' => $data['Total_cropKexport_an4'] ?? null,
+                'balance_N' => $data['balance_N_an4'] ?? null,  
+                'balance_P' => $data['balance_P_an4'] ?? null,
+                'balance_K' => $data['balance_K_an4'] ?? null,
             ]);
+
+            if (isset($data['additional_an3']) && $data['additional_an3'] === 'yes') {
+                NutrientBalance::create([
+                    'project_id' => $projectId,
+                    'farmer_field_id' => $farmerField->id,
+                    'year' => '2019',
+                    'erosion_level' => $data['erosion_level_an3'] ?? null,
+                    'erosion_amt_Tha' => $data['erosion_amt_Tha_an3'] ?? null,
+                    'erosionNloss_kgHa' => $data['erosionNloss_kgHa_an3'] ?? null,
+                    'erosionPloss_kgHa' => $data['erosionPloss_kgHa_an3'] ?? null,
+                    'erosionKloss_kgHa' => $data['erosionKloss_kgHa_an3'] ?? null,
+                    'tot_org_Ninput' => $data['tot_org_Ninput_an3'] ?? null,
+                    'tot_org_Pinput' => $data['tot_org_Pinput_an3'] ?? null,
+                    'tot_org_Kinput' => $data['tot_org_Kinput_an3'] ?? null,
+                    'tot_inorg_Ninput' => $data['tot_inorg_Ninput_an3'] ?? null,
+                    'tot_inorg_Pinput' => $data['tot_inorg_Pinput_an3'] ?? null,
+                    'tot_inorg_Kinput' => $data['tot_inorg_Kinput_an3'] ?? null,
+                    'Total_cropNexport' => $data['Total_cropNexport_an3'] ?? null,
+                    'Total_cropPexport' => $data['Total_cropPexport_an3'] ?? null,
+                    'Total_cropKexport' => $data['Total_cropKexport_an3'] ?? null,
+                    'balance_N' => $data['balance_N_an3'] ?? null,
+                    'balance_P' => $data['balance_P_an3'] ?? null,
+                    'balance_K' => $data['balance_K_an3'] ?? null,
+                ]);
+            }
+
+            if (isset($data['additional_an2']) && $data['additional_an2'] === 'yes') {
+                NutrientBalance::create([
+                    'project_id' => $projectId,
+                    'farmer_field_id' => $farmerField->id,
+                    'year' => '2018',
+                    'erosion_level' => $data['erosion_level_an2'] ?? null,
+                    'erosion_amt_Tha' => $data['erosion_amt_Tha_an2'] ?? null,
+                    'erosionNloss_kgHa' => $data['erosionNloss_kgHa_an2'] ?? null,
+                    'erosionPloss_kgHa' => $data['erosionPloss_kgHa_an2'] ?? null,
+                    'erosionKloss_kgHa' => $data['erosionKloss_kgHa_an2'] ?? null,
+                    'tot_org_Ninput' => $data['tot_org_Ninput_an2'] ?? null,
+                    'tot_org_Pinput' => $data['tot_org_Pinput_an2'] ?? null,
+                    'tot_org_Kinput' => $data['tot_org_Kinput_an2'] ?? null,
+                    'tot_inorg_Ninput' => $data['tot_inorg_Ninput_an2'] ?? null,
+                    'tot_inorg_Pinput' => $data['tot_inorg_Pinput_an2'] ?? null,
+                    'tot_inorg_Kinput' => $data['tot_inorg_Kinput_an2'] ?? null,
+                    'Total_cropNexport' => $data['Total_cropNexport_an2'] ?? null,
+                    'Total_cropPexport' => $data['Total_cropPexport_an2'] ?? null,
+                    'Total_cropKexport' => $data['Total_cropKexport_an2'] ?? null,
+                    'balance_N' => $data['balance_N_an2'] ?? null,
+                    'balance_P' => $data['balance_P_an2'] ?? null,
+                    'balance_K' => $data['balance_K_an2'] ?? null,
+                ]);
+            }
+
+            if (isset($data['additional_an1']) && $data['additional_an1'] === 'yes') {
+                NutrientBalance::create([
+                    'project_id' => $projectId,
+                    'farmer_field_id' => $farmerField->id,
+                    'year' => '2017',
+                    'erosion_level' => $data['erosion_level_an1'] ?? null,
+                    'erosion_amt_Tha' => $data['erosion_amt_Tha_an1'] ?? null,
+                    'erosionNloss_kgHa' => $data['erosionNloss_kgHa_an1'] ?? null,
+                    'erosionPloss_kgHa' => $data['erosionPloss_kgHa_an1'] ?? null,
+                    'erosionKloss_kgHa' => $data['erosionKloss_kgHa_an1'] ?? null,
+                    'tot_org_Ninput' => $data['tot_org_Ninput_an1'] ?? null,
+                    'tot_org_Pinput' => $data['tot_org_Pinput_an1'] ?? null,
+                    'tot_org_Kinput' => $data['tot_org_Kinput_an1'] ?? null,
+                    'tot_inorg_Ninput' => $data['tot_inorg_Ninput_an1'] ?? null,
+                    'tot_inorg_Pinput' => $data['tot_inorg_Pinput_an1'] ?? null,
+                    'tot_inorg_Kinput' => $data['tot_inorg_Kinput_an1'] ?? null,
+                    'Total_cropNexport' => $data['Total_cropNexport_an1'] ?? null,
+                    'Total_cropPexport' => $data['Total_cropPexport_an1'] ?? null,
+                    'Total_cropKexport' => $data['Total_cropKexport_an1'] ?? null,
+                    'balance_N' => $data['balance_N_an1'] ?? null,
+                    'balance_P' => $data['balance_P_an1'] ?? null,
+                    'balance_K' => $data['balance_K_an1'] ?? null,
+                ]);
+            }
+
         }
 
-        if (isset($data['additional_an2']) && $data['additional_an2'] === 'yes') {
-            NutrientBalance::create([
-                'project_id' => $projectId,
-                'farmer_field_id' => $farmerField->id,
-                'year' => '2018',
-                'erosion_level' => $data['erosion_level_an2'] ?? null,
-                'erosion_amt_Tha' => $data['erosion_amt_Tha_an2'] ?? null,
-                'erosionNloss_kgHa' => $data['erosionNloss_kgHa_an2'] ?? null,
-                'erosionPloss_kgHa' => $data['erosionPloss_kgHa_an2'] ?? null,
-                'erosionKloss_kgHa' => $data['erosionKloss_kgHa_an2'] ?? null,
-                'tot_org_Ninput' => $data['tot_org_Ninput_an2'] ?? null,
-                'tot_org_Pinput' => $data['tot_org_Pinput_an2'] ?? null,
-                'tot_org_Kinput' => $data['tot_org_Kinput_an2'] ?? null,
-                'tot_inorg_Ninput' => $data['tot_inorg_Ninput_an2'] ?? null,
-                'tot_inorg_Pinput' => $data['tot_inorg_Pinput_an2'] ?? null,
-                'tot_inorg_Kinput' => $data['tot_inorg_Kinput_an2'] ?? null,
-                'Total_cropNexport' => $data['Total_cropNexport_an2'] ?? null,
-                'Total_cropPexport' => $data['Total_cropPexport_an2'] ?? null,
-                'Total_cropKexport' => $data['Total_cropKexport_an2'] ?? null,
-                'balance_N' => $data['balance_N_an2'] ?? null,
-                'balance_P' => $data['balance_P_an2'] ?? null,
-                'balance_K' => $data['balance_K_an2'] ?? null,
-            ]);
-        }
-
-
-        if (isset($data['additional_an1']) && $data['additional_an1'] === 'yes') {
-            NutrientBalance::create([
-                'project_id' => $projectId,
-                'farmer_field_id' => $farmerField->id,
-                'year' => '2017',
-                'erosion_level' => $data['erosion_level_an1'] ?? null,
-                'erosion_amt_Tha' => $data['erosion_amt_Tha_an1'] ?? null,
-                'erosionNloss_kgHa' => $data['erosionNloss_kgHa_an1'] ?? null,
-                'erosionPloss_kgHa' => $data['erosionPloss_kgHa_an1'] ?? null,
-                'erosionKloss_kgHa' => $data['erosionKloss_kgHa_an1'] ?? null,
-                'tot_org_Ninput' => $data['tot_org_Ninput_an1'] ?? null,
-                'tot_org_Pinput' => $data['tot_org_Pinput_an1'] ?? null,
-                'tot_org_Kinput' => $data['tot_org_Kinput_an1'] ?? null,
-                'tot_inorg_Ninput' => $data['tot_inorg_Ninput_an1'] ?? null,
-                'tot_inorg_Pinput' => $data['tot_inorg_Pinput_an1'] ?? null,
-                'tot_inorg_Kinput' => $data['tot_inorg_Kinput_an1'] ?? null,
-                'Total_cropNexport' => $data['Total_cropNexport_an1'] ?? null,
-                'Total_cropPexport' => $data['Total_cropPexport_an1'] ?? null,
-                'Total_cropKexport' => $data['Total_cropKexport_an1'] ?? null,
-                'balance_N' => $data['balance_N_an1'] ?? null,
-                'balance_P' => $data['balance_P_an1'] ?? null,
-                'balance_K' => $data['balance_K_an1'] ?? null,
-            ]);
-        }
     }
 }
